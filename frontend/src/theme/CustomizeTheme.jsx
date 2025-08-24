a2da062035c883213c55d6f5f76cdc0df0cdea30
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-24 23:54:10 +0700

    [UPDATE] - Theme - Customize Theme

--- frontend/src/theme/CustomizeTheme.jsx
+++ frontend/src/theme/CustomizeTheme.jsx
@@ -6,31 +6,34 @@ export default function ThemeProvider({ children }) {
       theme={{
         token: {
           fontFamily: "Poppins, sans-serif",
-          colorPrimary: "#171717",
+          colorPrimary: "#171717",          
         },
         components: {
           Button: {
-            colorPrimary: "#171717",        
-            primaryColor: "#FDFDFD",       
-            contentFontSize: 16,           
-            fontWeight: 500,                
+            colorPrimary: "#171717",                        
+            primaryColor: "#FDFDFD",
+            contentFontSize: 16,
+            fontWeight: 500,
             borderRadius: 12,
             controlHeight: 52,
+            defaultBg: "#FDFDFD",
+            defaultColor: "#171717",
+            defaultBorderColor: "#E9E9E9",
+            defaultHoverBorderColor: "#717171",
+            defaultHoverColor: "#171717",
           },
           Collapse: {
-            
-            
-                  
             contentBg: "#FDFDFD",
-            
-            
-            
-            colorText: "#717171", 
-            colorTextHeading: "#FFFFFF", 
-            
-                        
-            
+            colorText: "#717171",
+            colorTextHeading: "#FFFFFF",
           },
+          Segmented: {
+            trackBg: "#FDFDFD",
+            itemSelectedColor: "#171717",
+            itemColor: "#717171",
+            itemActiveBg: "#F3F4F6",
+            borderRadius: 12
+          }
         },
       }}
     >
