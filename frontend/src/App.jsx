7d2ce14e6c8467328375f11ab20a6bd604b0caf9
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-24 23:51:49 +0700

    [UPDATE] - App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,9 @@
-import Banner from "./components/layout/public/Banner";
-import Client from "./components/layout/public/Client";
-import FAQ from "./components/layout/public/FAQ";
-import Features from "./components/layout/public/Features";
-import Navbar from "./components/layout/public/Navbar";
+import LandingPage from "./components/layout/public/LandingPage";
 
 const App = () => {
   return (
     <>    
-    <Navbar />
-    <section id="home">
-        <Banner />
-      </section>
-
-      {/* Section Client */}
-      <section id="client">
-        <Client />
-      </section>
-
-      <section id="features">
-        <Features />
-      </section>
-
-      <section id="faq">
-        <FAQ />
-      </section>
+    <LandingPage />   
     </>
   );
 };
