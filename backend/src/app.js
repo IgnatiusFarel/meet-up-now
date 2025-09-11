1f6c3ee015a93010486e5d806f174882c55ae156
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-09-11 12:21:30 +0700

    [UPDATE] - App Js

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,7 @@ const wsService = new WebSocketService(server);
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
 
