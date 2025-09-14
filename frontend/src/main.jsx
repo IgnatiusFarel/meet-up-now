645e396b0dd607049fddce4707e97dda18924575
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-09-14 23:39:42 +0700

    [UPDATE] - Main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,19 @@
+import "./index.css";
+import App from "./App.jsx";
 import { StrictMode } from "react";
+import { App as AntdApp } from "antd";
+import "@ant-design/v5-patch-for-react-19";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./theme/CustomizeTheme.jsx";
-import "@ant-design/v5-patch-for-react-19";
-import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-      <App />
+        <AntdApp>
+          <App />
+        </AntdApp>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
