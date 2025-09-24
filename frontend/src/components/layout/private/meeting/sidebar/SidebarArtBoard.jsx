c4130e28999c93054ee212f4b8678aa47f7ae354
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-09-25 03:12:29 +0700

    [ADD] - Components - Layout - Private - Meeting - Sidebar - Sidebar Art Board

---
+++ frontend/src/components/layout/private/meeting/sidebar/SidebarArtBoard.jsx
@@ -0,0 +1,9 @@
+const SidebarArtboard = () => {
+  return (
+    <div className="flex items-center justify-center h-full text-gray-500">
+      <p>Artboard / Whiteboard coming soon...</p>
+    </div>
+  );
+};
+
+export default SidebarArtboard;
