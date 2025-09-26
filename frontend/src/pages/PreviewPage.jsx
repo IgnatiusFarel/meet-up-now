bd2463d39e63ea16895ec3fa8b03b048bf70cc7f
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-09-26 23:56:31 +0700

    [UPDATE] - Pages - Preview Page

--- frontend/src/pages/PreviewPage.jsx
+++ frontend/src/pages/PreviewPage.jsx
@@ -4,22 +4,20 @@ import Header from "@/components/layout/private/preview/Header";
 const PreviewPage = () => {
   return (
     <>
-      <head>
-        <title>Preview | Meet Up Now</title>
-        <meta
-          name="description"
-          content="Preview your camera and microphone before joining a meeting on Meet Up Now. Choose your devices, turn on or off camera and mic, and get ready to collaborate."
-        />
-        <link rel="icon" href="/MeetUpNow.png" />
+      <title>Preview | Meet Up Now</title>
+      <meta
+        name="description"
+        content="Preview your camera and microphone before joining a meeting on Meet Up Now. Choose your devices, turn on or off camera and mic, and get ready to collaborate."
+      />
+      <link rel="icon" href="/MeetUpNow.png" />
 
-        <meta property="og:title" content="Preview | Meet Up Now" />
-        <meta
-          property="og:description"
-          content="Preview your camera and microphone before joining a meeting on Meet Up Now. Choose your devices, turn on or off camera and mic, and get ready to collaborate."
-        />
-        <meta property="og:image" content="/MeetUpNow.png" />
-        <meta property="og:type" content="website" />
-      </head>
+      <meta property="og:title" content="Preview | Meet Up Now" />
+      <meta
+        property="og:description"
+        content="Preview your camera and microphone before joining a meeting on Meet Up Now. Choose your devices, turn on or off camera and mic, and get ready to collaborate."
+      />
+      <meta property="og:image" content="/MeetUpNow.png" />
+      <meta property="og:type" content="website" />
 
       <main className="min-h-screen bg-white">
         <Header />
