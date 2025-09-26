4fbfb47471bb2931f74958a08e37658c31a61f77
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-09-26 23:56:02 +0700

    [UPDATE] - Pages - Dashboard Page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -4,15 +4,24 @@ import Carousels from "@/components/layout/private/dashboard/Carousels";
 
 const DashboardPage = () => {
   return (
-    <>      
-    <head>
-        <title>Dashboard | Meet Up Now</title>
-        <meta
-          name="description"
-          content="Meet Up Now dashboard page, where you can create meetings and join meetings."
-        />
-        <link rel="icon" href="/MeetUpNow.png" />
-      </head>
+    <>
+      <title>Dashboard | Meet Up Now</title>
+      <meta
+        name="description"
+        content="Meet Up Now dashboard page, where you can create meetings and join meetings."
+      />
+      <link rel="icon" href="/MeetUpNow.png" />
+
+      <meta
+        property="og:title"
+        content="Meet Up Now | Connect and Join Meetings Effortlessly"
+      />
+      <meta
+        property="og:description"
+        content="Meet Up Now is the easiest way to connect, join, and manage your online meetings. Stay productive and collaborate seamlessly."
+      />
+      <meta property="og:image" content="/MeetUpNow.png" />
+      <meta property="og:type" content="website" />
 
       <main className="min-h-screen bg-white">
         <Header />
